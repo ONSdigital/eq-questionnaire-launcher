05dba4e6d24d6c74071fccd5bd4e9a85098ecb47
rmccar <42928680+rmccar@users.noreply.github.com>
2024-08-12 14:39:46 +0100

    Fix readme dockerfile and JS formatting (#89)

--- static/javascript/launch.js
+++ static/javascript/launch.js
@@ -81,7 +81,7 @@
             ].join("");
           }
           for (var t = [], i = 0; i < 256; ++i)
-            t[i] = (i + 256).toString(16).substr(1);
+            t[i] = (i + 256).toString(16).substring(1);
           n.exports = o;
         },
         {},
@@ -90,8 +90,7 @@
         function (e, n, r) {
           var o =
             ("undefined" != typeof crypto &&
-              crypto.getRandomValues &&
-              crypto.getRandomValues.bind(crypto)) ||
+              crypto.getRandomValues?.bind(crypto)) ||
             ("undefined" != typeof msCrypto &&
               "function" == typeof window.msCrypto.getRandomValues &&
               msCrypto.getRandomValues.bind(msCrypto));
@@ -213,17 +212,17 @@ function showMetadataAccordion(type, show) {
   }
 }
 
-function enableButtons(button) {
-  for (let i = 0; i < button.length; i++) {
-    button[i].classList.remove("ons-btn--disabled");
-    button[i].disabled = false;
+function enableButtons(buttons) {
+  for (const button of buttons) {
+    button.classList.remove("ons-btn--disabled");
+    button.disabled = false;
   }
 }
 
-function disableButtons(button) {
-  for (let i = 0; i < button.length; i++) {
-    button[i].classList.add("ons-btn--disabled");
-    button[i].disabled = true;
+function disableButtons(buttons) {
+  for (const button of buttons) {
+    button.classList.add("ons-btn--disabled");
+    button.disabled = true;
   }
 }
 
@@ -250,9 +249,9 @@ function includeSurveyMetadataFields(schema_name, survey_type) {
 
   document.querySelector("#survey_metadata_fields").innerHTML =
     `<div class="ons-field ons-field--inline">
-                    <label class="ons-label" for="form_type">form_type</label>
-                    <input id="form_type" name="form_type" type="text" value="${formTypeValue}" class="ons-input ons-input--text ons-input-type__input">
-                </div>`;
+      <label class="ons-label" for="form_type">form_type</label>
+      <input id="form_type" name="form_type" type="text" value="${formTypeValue}" class="ons-input ons-input--text ons-input-type__input">
+    </div>`;
   setTabIndex("survey_type_metadata_detail", 0);
   showMetadataAccordion("sds", true);
 }
@@ -452,7 +451,6 @@ function loadSchemaMetadata(schemaName, schemaUrl, cirInstrumentId) {
   setTabIndex("sds_metadata_detail", -1);
   getDataAsync(survey_data_url)
     .then((schema_response) => {
-      document.querySelector("#survey_metadata").innerHTML = "";
       document.querySelector("#survey_metadata").innerHTML = "";
 
       // We always need survey_id from top-level schema metadata for SDS retrieval
